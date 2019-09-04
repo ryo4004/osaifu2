067caa449a79968d3480375868b7bcd3df36b5ca
ryo4004 <regs.ryo4004@gmail.com>
2019-09-04 18:07:15 +0900

    Update list.js

--- server/list.js
+++ server/list.js
@@ -12,15 +12,20 @@ function getDBStatus (user, callback) {
   console.log('[listDB] getDBStatus: ' + user.userid)
   listDB.findOne({'host': user.userKey}, (hostError, host) => {
     if (hostError) return callback({type: 'hostDBError', fatal: true}, null)
-    if (host !== null) {
-      if (!host.status) return callback({type: 'statusError', fatal: false}, null)
-      return callback(null, {dbStatus: host.status, dbKey: host.dbKey, user: 'host'})
-    }
     listDB.findOne({'client': user.userKey}, (clientError, client) => {
       if (clientError) return callback({type: 'clientDBError', fatal: true}, null)
-      if (client === null) return callback({type: 'clientNotFound', fatal: false}, null)
-      if (!client.status) return callback({type: 'clientDisabled', fatal: false}, null)
-      return callback(null, {dbStatus: client.status, dbKey: client.dbKey, user: 'client'})
+      if (host === null && client === null) return callback({type: 'notFound', fatal: false}, null)
+      if (host) {
+        if (!host.status) return callback({type: 'statusError', fatal: false}, null)
+        return callback(null, {dbStatus: host.status, dbKey: host.dbKey, user: 'host'})
+      } else if (client) {
+        if (!client.status) return callback({type: 'clientDisabled', fatal: false}, null)
+        return callback(null, {dbStatus: client.status, dbKey: client.dbKey, user: 'client'})
+      } else if (host === null) {
+        return callback({type: 'hostNotFound', fatal: false}, null)
+      } else if (client === null) {
+        return callback({type: 'clientNotFound', fatal: false}, null)
+      }
     })
   })
 }

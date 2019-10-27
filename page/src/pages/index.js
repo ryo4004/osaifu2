87be0749d7117f87039c4cf6c80bafaced680a9e
ryo4004 <regs.ryo4004@gmail.com>
2019-10-28 07:53:59 +0900

    Update index.js

--- page/src/pages/index.js
+++ page/src/pages/index.js
@@ -2,19 +2,28 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+
+import './index.scss'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO title="おさいふ" />
+    <div className='index'>
+      <div className='top'>
+        <div className='title'>
+          <h1>おさいふ</h1>
+          <p>ふたりでつける家計簿アプリ</p>
+        </div>
+      </div>
+      
+      <div className='features'>
+        <h2>特徴</h2>
+        
+      </div>
+      <Link to="/page-2">Go to page 2</Link>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 

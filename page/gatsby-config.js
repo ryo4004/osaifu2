4beb2f031e83c54ccdcf3bd13f8ed74bdf2df71b
ryo4004 <regs.ryo4004@gmail.com>
2019-10-28 07:53:08 +0900

    Update gatsby-config.js

--- page/gatsby-config.js
+++ page/gatsby-config.js
@@ -21,10 +21,10 @@ module.exports = {
         name: 'ふたりでつける家計簿アプリ',
         short_name: 'おさいふ',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#f8ab86',
+        theme_color: '#f8ab86',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/osaifu-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

4d12b466dd817ee8d119c436d8e21065c5eed791
ryo4004 <regs.ryo4004@gmail.com>
2019-11-11 17:48:32 +0900

    Update header.js

--- page/src/components/header.js
+++ page/src/components/header.js
@@ -2,13 +2,22 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import Image from './osaifu-icon'
+
+import './header.scss'
+
 const Header = ({ siteTitle }) => (
-  <header>
-    {/* <h1 style={{ margin: 0 }}>
-      <Link to="/">
-        {siteTitle}
-      </Link>
-    </h1> */}
+  <header className='header'>
+    <div className='frame'>
+      <div className='title'>
+        <div className='icon'>
+          <Image className='img' />
+        </div>
+        <h1 style={{ margin: 0 }}>
+          <Link to="/">{siteTitle}</Link>
+        </h1>
+      </div>
+    </div>
   </header>
 )
 

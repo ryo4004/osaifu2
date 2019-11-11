f0cd6e79df96604a5eae51825b62cb538c8861f2
ryo4004 <regs.ryo4004@gmail.com>
2019-11-11 17:47:46 +0900

    Update index.js

--- page/src/pages/index.js
+++ page/src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-// import Image from "../components/image"
 import SEO from "../components/seo"
 
 import './index.scss'
@@ -18,11 +16,45 @@ const IndexPage = () => (
         </div>
       </div>
       
-      <div className='features'>
+      <section className='features'>
         <h2>特徴</h2>
-        
-      </div>
-      <Link to="/page-2">Go to page 2</Link>
+        <div className='first-feature feature'>
+          <h3>お金のことで揉めない</h3>
+          <p>
+            ふたりで過ごすためのお金をどちらがどれだけ負担するのか、たびたびもめることがあります。
+            ふたりの価値観にもよりますが、割り勘にするのか、どこまで負担するのか。
+            いっしょに暮らし始めると日用品や家賃など支払いも増え、清算するのもひと手間になってしまいます。
+            おさいふはそんなふたりのためのお金の管理ツールです。
+          </p>
+        </div>
+
+        <div className='second-feature feature'>
+          <h3>ふたりでいっしょに記録</h3>
+          <p>
+            このアプリではふたりのスマホそれぞれからひとつのデータベースに記録できます。
+            買い物をしたらすぐに記録。
+            彼がお金を払ったら彼女が記録。
+            お互いが入力した記録をそれぞれのスマホで確認できます。
+          </p>
+        </div>
+
+        <div className='third-feature feature'>
+          <h3>記録はかんたん</h3>
+          <p>
+            項目は「金額」さえあればOK。「記録日」や「メモ」も追加できます。
+            また、それぞれがお金を出し合って購入した場合はそれも記録できます。
+          </p>
+        </div>
+
+        <div className='fourth-feature feature'>
+          <h3>こんなひとに</h3>
+          <ul>
+            <li>おごるのが当たり前でないひと</li>
+            <li>これから同棲しようとしているひと</li>
+            <li>お金の管理がずさんになりがちなひと</li>
+          </ul>
+        </div>
+      </section>
     </div>
   </Layout>
 )

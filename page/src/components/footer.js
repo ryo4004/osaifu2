f8c5b3c97e322a257cfbdd4aad0990224f661679
ryo4004 <regs.ryo4004@gmail.com>
2019-11-12 23:50:26 +0900

    page update footer.js

--- page/src/components/footer.js
+++ page/src/components/footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import './footer.scss'
 
 const Footer = () => (
   <footer className='footer'>
-    <div className='frame'>
+    <div className='contents'>
       <small>&copy; {new Date().getFullYear()} おさいふ</small>
+      <div className='links'>
+        <Link to='/'>ホーム</Link>
+        <Link to='/terms'>利用規約</Link>
+        <Link to='/privacy'>個人情報保護方針</Link>
+      </div>
     </div>
   </footer>
 )

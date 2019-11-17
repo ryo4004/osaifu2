f65590daca888c0a89fc6df16e1332fa2e64d180
ryo4004 <regs.ryo4004@gmail.com>
2019-11-18 08:09:15 +0900

    page update footer.js

--- page/src/components/footer.js
+++ page/src/components/footer.js
@@ -6,12 +6,14 @@ import './footer.scss'
 const Footer = () => (
   <footer className='footer'>
     <div className='contents'>
-      <small>&copy; {new Date().getFullYear()} おさいふ</small>
       <div className='links'>
         <Link to='/'>ホーム</Link>
+        <Link to='/features'>特徴</Link>
+        <Link to='/guide'>ご利用方法</Link>
         <Link to='/terms'>利用規約</Link>
         <Link to='/privacy'>個人情報保護方針</Link>
       </div>
+      <small>&copy; {new Date().getFullYear()} おさいふ</small>
     </div>
   </footer>
 )

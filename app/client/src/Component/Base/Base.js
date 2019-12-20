d193ac0e85f0b8287656d9d0f138f5b478c1f93a
ryo4004 <regs.ryo4004@gmail.com>
2019-12-20 10:47:07 +0900

    app update Base Component

--- app/client/src/Component/Base/Base.js
+++ app/client/src/Component/Base/Base.js
@@ -24,21 +24,23 @@ const Base = () => {
 
   return (
     <div className='base'>
-      <header>
-        <div className='contents'>
-          <div className='title'>
-            <a href='https://osaifu.zatsuzen.com'>
-              <div className='icon'><Logo /></div><h1>おさいふ</h1>
-            </a>
+      <div className='header-base'>
+        <header>
+          <div className='contents'>
+            <div className='title'>
+              <a href='https://osaifu.zatsuzen.com'>
+                <div className='icon'><Logo /></div><h1>おさいふ</h1>
+              </a>
+            </div>
+            <div className='menu'>
+              <a href='https://osaifu.zatsuzen.com'>ホーム</a>
+              {/* <a href='https://osaifu.zatsuzen.com/feature'>特徴</a> */}
+              <a href='https://osaifu.zatsuzen.com/guide'>ご利用方法</a>
+              {link}
+            </div>
           </div>
-          <div className='menu'>
-            {/* <a href='https://osaifu.zatsuzen.com'>ホーム</a> */}
-            {/* <a href='https://osaifu.zatsuzen.com/feature'>特徴</a> */}
-            {/* <a href='https://osaifu.zatsuzen.com/guide'>ご利用方法</a> */}
-            {link}
-          </div>
-        </div>
-      </header>
+        </header>
+      </div>
       <Switch>
         <Route path='/login' component={Login} />
         <Route path='/signup' component={Signup} />
